28181c334d2ad374a455bedd1666c497f1cc52fa
Joshua Shuttleworth <joshua.shuttleworth22@ycdsbk12.ca>
2021-04-30 14:18:51 -0400

    Did question 5 and added Run.sh to speed up testing

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -1,4 +1,4 @@
-//package gr11review.part1;
+package gr11review.part1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,7 +7,7 @@ import java.text.DecimalFormat;
 public class Review4{
     public static void main(String[] args) throws IOException{
         int N;
-        double Total=0;
+        double total=0;
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         String pattern="###,##0.00";
         DecimalFormat df=new DecimalFormat(pattern);
@@ -15,10 +15,10 @@ public class Review4{
         N = Integer.parseInt(br.readLine());
         for(int i=0;i<N;i++){
             System.out.print("Enter the price for item "+(i+1)+": ");
-            Total += Double.parseDouble(br.readLine());
+            total += Double.parseDouble(br.readLine());
         }
-        System.out.println("Subtotal: $"+df.format(Total));
-        System.out.println("Tax: $"+(df.format(Total*0.13)));
-        System.out.println("Total: $"+(df.format(Total*1.13)));
+        System.out.println("Subtotal: $"+df.format(total));
+        System.out.println("Tax: $"+(df.format(total*0.13)));
+        System.out.println("Total: $"+(df.format(total*1.13)));
     }   
 }
--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,1 +1,25 @@
 package gr11review.part1;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Review5{
+    public static void main(String[] args) throws IOException{
+        int years=0;
+        double invested,intrest,target,total=0.00;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Enter the yearly invested amount: ");
+        invested=Double.parseDouble(br.readLine());
+        System.out.print("Enter the compound interest rate: ");
+        intrest=Double.parseDouble(br.readLine());
+        System.out.print("Enter the target amount: ");
+        target=Double.parseDouble(br.readLine());
+        intrest=intrest/100;
+        while(target>total){
+            total+=invested;
+            total+=(intrest*total);
+            years++;
+        }
+        System.out.println("The target amount will be earned in "+years+" years.");
+    }   
+}
